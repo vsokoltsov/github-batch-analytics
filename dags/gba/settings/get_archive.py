ace71663045b20f6627260637e2ce6b8f6dd7bd5
Vadim Sokoltsov <vforvad@gmail.com>
2026-03-08 23:22:13 +0100

    Add settings for get_archive step

---
+++ dags/gba/settings/get_archive.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class GetArchiveSettings(BaseSettings):
+    S3_LANDING_ZONE_BUCKET_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+@lru_cache(maxsize=1)
+def get_download_archive_settings() -> GetArchiveSettings:
+    return GetArchiveSettings()  # type: ignore[call-arg]
