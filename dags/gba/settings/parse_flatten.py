501f64cd6d956449fa4ba5b7293205ddcd65bf0a
Vadim Sokoltsov <vforvad@gmail.com>
2026-03-12 23:06:38 +0100

    Add settings for parse_flatten step

---
+++ dags/gba/settings/parse_flatten.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class ParseFlattenSettings(BaseSettings):
+    S3_BRONZE_ZONE_BUCKET_NAME: str
+    AWS_PROFILE: str
+    AWS_CONFIG_FILE: str
+    AWS_SHARED_CREDENTIALS_FILE: str
+    SPARK_MASTER_URL: str
+
+@lru_cache(maxsize=1)
+def get_parse_flatten_settings() -> ParseFlattenSettings:
+    return ParseFlattenSettings() # type: ignore[call-arg]
