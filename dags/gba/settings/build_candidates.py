80a86f347df712f2d6744ac240d31934211d60ee
Vadim Sokoltsov <vforvad@gmail.com>
2026-03-21 22:04:27 +0100

    Add build candidates settings

---
+++ dags/gba/settings/build_candidates.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+from enum import Enum
+
+class CandidatesType(Enum):
+    REPO = "repo"
+    ORG = "org"
+
+
+class BuildCandidateSettings(BaseSettings):
+    S3_SILVER_ZONE_BUCKET_NAME: str
+    AWS_PROFILE: str
+    AWS_CONFIG_FILE: str
+    AWS_SHARED_CREDENTIALS_FILE: str
+    SPARK_MASTER_URL: str
+
+
+@lru_cache(maxsize=1)
+def get_build_candidates_settings() -> BuildCandidateSettings:
+    return BuildCandidateSettings()  # type: ignore[call-arg]
