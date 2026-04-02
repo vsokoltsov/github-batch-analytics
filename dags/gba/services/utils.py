a8903a6c8d804b476a2edaaceb0d4f81533e7a33
Vadim Sokoltsov <vforvad@gmail.com>
2026-04-02 23:50:16 +0200

    Move common functions to utils.py

--- dags/gba/services/utils.py
+++ dags/gba/services/utils.py
@@ -11,3 +11,13 @@ def to_s3a(path: str) -> str:
     if path.startswith("s3://"):
         return "s3a://" + path[len("s3://") :]
     return path
+
+
+def to_s3(path: str) -> str:
+    if path.startswith("s3a://"):
+        return "s3://" + path[len("s3a://") :]
+    if path.startswith("s3://"):
+        return path
+    if "://" not in path and "/" in path:
+        return f"s3://{path.lstrip('/')}"
+    return path
