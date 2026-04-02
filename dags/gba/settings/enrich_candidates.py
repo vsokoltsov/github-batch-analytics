ea3bf931313a756faed9310efc07a9cfc2317c53
Vadim Sokoltsov <vforvad@gmail.com>
2026-04-02 23:50:42 +0200

    Add enrich candidates settings

---
+++ dags/gba/settings/enrich_candidates.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+
+class EnrichCandidatesSettings(BaseSettings):
+    S3_BRONZE_ZONE_BUCKET_NAME: str
+    AWS_PROFILE: str
+    AWS_CONFIG_FILE: str
+    AWS_SHARED_CREDENTIALS_FILE: str
+
+
+@lru_cache(maxsize=1)
+def get_enrich_candidates_settings() -> EnrichCandidatesSettings:
+    return EnrichCandidatesSettings()  # type: ignore[call-arg]
