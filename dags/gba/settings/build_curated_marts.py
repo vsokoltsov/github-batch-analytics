5aea83e894220ec0acb6c1112a65cb8683742786
Vadim Sokoltsov <vforvad@gmail.com>
2026-04-08 11:03:21 +0200

    Add settings for build curated marts

---
+++ dags/gba/settings/build_curated_marts.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+
+class BuildMartsSettings(BaseSettings):
+    S3_MARTS_BUCKET_NAME: str
+    AWS_PROFILE: str
+    AWS_CONFIG_FILE: str
+    AWS_SHARED_CREDENTIALS_FILE: str
+    SPARK_MASTER_URL: str
+
+
+@lru_cache(maxsize=1)
+def get_build_candidates_settings() -> BuildMartsSettings:
+    return BuildMartsSettings()  # type: ignore[call-arg]
